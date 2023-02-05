80022054be810d98779692d0e3c203372ddb343c
Ethan Jacobs <jacobethan1992@gmail.com>
2023-02-05 18:58:17 -0500

    exercise

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+
+
+/////////Exercise Two/////////
+Console.WriteLine("What is your favorite subject");
+string subject = Console.ReadLine().ToLower();
+switch (subject)
+{
+    case "math":
+        Console.WriteLine("No not my favorite");
+        break;
+    case "reading":
+        Console.WriteLine("this troubles me");
+        break;
+    case "history":
+        Console.WriteLine("It's overwhelimg");
+        break;
+    case "social studies":
+        Console.WriteLine("I love this");
+        break;
+    case "science":
+        Console.WriteLine("i hate this");
+        break;
+    default:
+        Console.WriteLine("nope not at all");
+        break;
+
+
+}
+
+
+
+
+
+
+        
